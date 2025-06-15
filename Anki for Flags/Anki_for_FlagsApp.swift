112b600fc738bbe98c339718c51f8f13b8c90664
Harlech1 <turkerkizilcik@gmail.com>
2025-06-15 14:28:19 +0300

    initial commit

---
+++ Anki for Flags/Anki_for_FlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anki_for_FlagsApp.swift
+//  Anki for Flags
+//
+//  Created by Türker Kızılcık on 15.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Anki_for_FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Anki for Flags/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Anki for Flags
+//
+//  Created by Türker Kızılcık on 15.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
